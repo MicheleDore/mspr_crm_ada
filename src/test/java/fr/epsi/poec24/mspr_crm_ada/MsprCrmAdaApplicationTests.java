8592a93686a3a997cbea5ed7665c2a76357fa4c2
MicheleDore <micheledore3@gmail.com>
2024-02-24 16:25:42 +0100

    Project created

---
+++ src/test/java/fr/epsi/poec24/mspr_crm_ada/MsprCrmAdaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.epsi.poec24.mspr_crm_ada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsprCrmAdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
