75ea974d96e01ffd63b7feb1ebfeb04653204230
MicheleDore <micheledore3@gmail.com>
2024-02-24 16:25:42 +0100

    Project created

---
+++ src/main/java/fr/epsi/poec24/mspr_crm_ada/MsprCrmAdaApplication.java
@@ -0,0 +1,13 @@
+package fr.epsi.poec24.mspr_crm_ada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsprCrmAdaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsprCrmAdaApplication.class, args);
+    }
+
+}
