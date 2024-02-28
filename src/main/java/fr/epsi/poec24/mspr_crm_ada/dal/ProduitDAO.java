de476d9c6b9eb07328a16d56427339c0f7f20927
Mickaël Abin <abin.mickael@gmail.com>
2024-02-28 11:05:25 +0100

    Produit et spring Security

--- src/main/java/fr/epsi/poec24/mspr_crm_ada/dal/ProduitDAO.java
+++ src/main/java/fr/epsi/poec24/mspr_crm_ada/dal/ProduitDAO.java
@@ -2,6 +2,8 @@ package fr.epsi.poec24.mspr_crm_ada.dal;
 
 import fr.epsi.poec24.mspr_crm_ada.domain.Produit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProduitDAO extends JpaRepository<Produit, Integer> {
 }
