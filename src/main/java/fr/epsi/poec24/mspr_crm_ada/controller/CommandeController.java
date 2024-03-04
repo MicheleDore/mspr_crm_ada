b886e08b3a5f7d25038b90ed44ad1c3de36f2925
Mickaël Abin <abin.mickael@gmail.com>
2024-03-04 11:38:09 +0100

    fonctionnalités produits ok sauf nb vente car je dois faire commande avant pour avoir le lien avec produit

---
+++ src/main/java/fr/epsi/poec24/mspr_crm_ada/controller/CommandeController.java
@@ -0,0 +1,31 @@
+package fr.epsi.poec24.mspr_crm_ada.controller;
+
+import fr.epsi.poec24.mspr_crm_ada.domain.Commande;
+import fr.epsi.poec24.mspr_crm_ada.service.CommandeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/commandes")
+public class CommandeController {
+    private CommandeService service;
+
+
+    @Autowired
+    public CommandeController(CommandeService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public String afficherListeCommande(Model model) {
+
+        List<Commande> mesCommandes = service.findAll();
+        model.addAttribute("commandes", mesCommandes);
+        System.out.println(mesCommandes);
+        return "view-commandes-list";
+    }
+}
---
+++ src/main/java/fr/epsi/poec24/mspr_crm_ada/service/CommandeService.java
@@ -0,0 +1,21 @@
+package fr.epsi.poec24.mspr_crm_ada.service;
+
+
+import fr.epsi.poec24.mspr_crm_ada.dal.CommandeDAO;
+import fr.epsi.poec24.mspr_crm_ada.dal.ProduitDAO;
+import fr.epsi.poec24.mspr_crm_ada.domain.Commande;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommandeService {
+    private CommandeDAO dao;
+    @Autowired
+    public CommandeService(CommandeDAO dao){ this.dao = dao;}
+
+    public List<Commande> findAll() {
+        return dao.findAll();
+    }
+}
