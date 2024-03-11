5b255afa3b8cc6d3d73205530cede7f826090be0
MicheleDore <micheledore3@gmail.com>
2024-03-11 16:04:40 +0100

    Création des fichiers stats

---
+++ src/main/java/fr/epsi/poec24/mspr_crm_ada/service/StatistiquesService.java
@@ -0,0 +1,15 @@
+package fr.epsi.poec24.mspr_crm_ada.service;
+
+import fr.epsi.poec24.mspr_crm_ada.dal.ProduitDAO;
+import fr.epsi.poec24.mspr_crm_ada.dal.StatistiquesDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StatistiquesService {
+
+    private StatistiquesDAO dao;
+
+//    @Autowired
+//    public StatistiquesService(StatistiquesDAO dao){ this.dao = dao;}
+}
