113b6501dc4af0026714e3f0abeee8cb9cdcfbcd
MicheleDore <micheledore3@gmail.com>
2024-03-13 12:26:07 +0100

    Récuperation et affichage chiffres d'affaire 2023 triés par mois

--- src/main/java/fr/epsi/poec24/mspr_crm_ada/service/StatistiquesService.java
+++ src/main/java/fr/epsi/poec24/mspr_crm_ada/service/StatistiquesService.java
@@ -1,15 +1,21 @@
 package fr.epsi.poec24.mspr_crm_ada.service;
 
-import fr.epsi.poec24.mspr_crm_ada.dal.ProduitDAO;
 import fr.epsi.poec24.mspr_crm_ada.dal.StatistiquesDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StatistiquesService {
 
     private StatistiquesDAO dao;
 
-//    @Autowired
-//    public StatistiquesService(StatistiquesDAO dao){ this.dao = dao;}
+    @Autowired
+    public StatistiquesService(StatistiquesDAO dao){ this.dao = dao;}
+
+    public List<Integer> calculCA(String annee) {
+        return dao.calculCA(annee);
+    }
+
 }
