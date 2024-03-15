31b13945056259dbdb764f04887ca5a8dd4dcf16
Mickaël Abin <abin.mickael@gmail.com>
2024-03-15 12:28:49 +0100

    dernier chagement security statistique

--- src/main/java/fr/epsi/poec24/mspr_crm_ada/configuration/SpringSecurityConfig.java
+++ src/main/java/fr/epsi/poec24/mspr_crm_ada/configuration/SpringSecurityConfig.java
@@ -19,19 +19,20 @@ public class SpringSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable().authorizeHttpRequests(auth ->{
-            auth.requestMatchers("/commandes").hasAnyRole("COMMERCIAUX","DIR-COM","RES-CAT");
-            auth.requestMatchers("/commandes/{id}/detail").hasAnyRole("COMMERCIAUX","DIR-COM","RES-CAT");
+            auth.requestMatchers("/statistiques/page").hasRole("DIR-COM");
+            auth.requestMatchers("/commandes").permitAll();
+            auth.requestMatchers("/commandes/{id}/detail").permitAll();
             auth.requestMatchers("/commandes/{id}/creer").hasAnyRole("COMMERCIAUX","DIR-COM");
             auth.requestMatchers("/Clients").permitAll();
             auth.requestMatchers("/Clients/creer").hasAnyRole("COMMERCIAUX","DIR-COM");
             auth.requestMatchers("/Clients/{id}/edition").hasAnyRole("COMMERCIAUX","DIR-COM");
-            auth.requestMatchers("/Clients/{id}/suppression").hasAnyRole("DIR-COM");
-            auth.requestMatchers("/Clients/{id}/detail").hasAnyRole("COMMERCIAUXADMIN","DIR-COM","RES-CAT");
+            auth.requestMatchers("/Clients/{id}/suppression").hasRole("DIR-COM");
+            auth.requestMatchers("/Clients/{id}/detail").permitAll();
             auth.requestMatchers("/produits").permitAll();
-            auth.requestMatchers("/produits/creer").hasAnyRole("RES-CAT");
-            auth.requestMatchers("/produits/{id}/edition").hasAnyRole("RES-CAT");
-            auth.requestMatchers("/produits/{id}/edition-prix").hasAnyRole("RES-CAT");
-            auth.requestMatchers("/produits/{id}/suppression").hasAnyRole("RES-CAT");
+            auth.requestMatchers("/produits/creer").hasRole("RES-CAT");
+            auth.requestMatchers("/produits/{id}/edition").hasRole("RES-CAT");
+            auth.requestMatchers("/produits/{id}/edition-prix").hasRole("RES-CAT");
+            auth.requestMatchers("/produits/{id}/suppression").hasRole("RES-CAT");
             auth.requestMatchers("/produits/{id}/detail").permitAll();
             auth.anyRequest().authenticated();
         } ).formLogin(Customizer.withDefaults()).build();
